5a05841451b690a069007f09e4246142012be7b5
Az <todortodoroff89@gmail.com>
2021-09-14 23:01:57 +0300

    update

--- src/StacksAndQueues01/Lab/SimpleCalculator02.java
+++ src/StacksAndQueues01/Lab/SimpleCalculator02.java
@@ -1,12 +1,32 @@
 package StacksAndQueues01.Lab;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class SimpleCalculator02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<String> equation = new ArrayDeque<>();
 
+        String[] input = scanner.nextLine().split("\\s+");
+        Collections.addAll(equation, input);
 
+        while (equation.size() > 1) {
+            int first = Integer.parseInt(equation.pop());
+            String operator = equation.pop();
+            int second = Integer.parseInt(equation.pop());
+
+            switch (operator) {
+                case "+":
+                    equation.push(String.valueOf(first + second));
+                    break;
+                case "-":
+                    equation.push(String.valueOf(first-second));
+                    break;
+            }
+        }
+        System.out.println(equation.pop());
     }
 }
