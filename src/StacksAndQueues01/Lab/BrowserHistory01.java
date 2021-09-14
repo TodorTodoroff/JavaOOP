55d5ae8d21781faa8ce3888ebe4f0e95f96456d9
Az <todortodoroff89@gmail.com>
2021-09-14 18:47:47 +0300

    update

---
+++ src/StacksAndQueues01/Lab/BrowserHistory01.java
@@ -0,0 +1,11 @@
+package StacksAndQueues01.Lab;
+
+import java.util.Scanner;
+
+public class BrowserHistory01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
