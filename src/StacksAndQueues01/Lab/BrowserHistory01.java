50136868335cbd77035b3cd2db039d481f373b5f
Az <todortodoroff89@gmail.com>
2021-09-14 22:15:22 +0300

    update

--- src/StacksAndQueues01/Lab/BrowserHistory01.java
+++ src/StacksAndQueues01/Lab/BrowserHistory01.java
@@ -1,11 +1,34 @@
 package StacksAndQueues01.Lab;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BrowserHistory01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<String> history = new ArrayDeque<>();
 
+        String input = scanner.nextLine();
+        String current = "";
+
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (!history.isEmpty()) {
+                    current = history.pop();
+                } else {
+                    System.out.println("no previous URLs");
+                    input = scanner.nextLine();
+                    continue;
+                }
+            } else {
+                if (!current.equals("")){
+                    history.push(current);
+                }
+                current = input;
+            }
+            System.out.println(current);
+            input = scanner.nextLine();
+        }
     }
 }
---
+++ src/StacksAndQueues01/Lab/SimpleCalculator02.java
@@ -0,0 +1,12 @@
+package StacksAndQueues01.Lab;
+
+import java.util.Scanner;
+
+public class SimpleCalculator02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
