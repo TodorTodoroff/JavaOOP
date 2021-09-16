ecbeea4f811f33ecd8bda8cda2105620bb01d0b5
Az <todortodoroff89@gmail.com>
2021-09-16 21:27:14 +0300

    update

--- src/StacksAndQueues01/Excercise/RecursiveFibonacci06.java
+++ src/StacksAndQueues01/Excercise/RecursiveFibonacci06.java
@@ -1,12 +1,28 @@
 package StacksAndQueues01.Excercise;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class RecursiveFibonacci06 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        ArrayDeque<Long> fibonacci = new ArrayDeque<>();
+        long N = Integer.parseInt(input.nextLine());
+        System.out.println(getFibonacci(fibonacci, N));
+    }
 
-
-
+    private static Long getFibonacci(ArrayDeque<Long> Fibonacci, Long N) {
+        if (N == 1) {
+            return 1L;
+        } else {
+            Fibonacci.offer(0L);
+            Fibonacci.offer(1L);
+            for (int i = 0; i < N; i++) {
+                long Sum = Fibonacci.poll() + Fibonacci.peek();
+                Fibonacci.offer(Sum);
+            }
+        }
+        Fibonacci.poll();
+        return Fibonacci.peek();
     }
 }
