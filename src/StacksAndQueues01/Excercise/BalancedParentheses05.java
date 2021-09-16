f914fceb6498bbb0e914d45b2bd22cbe61e0f0a4
Az <todortodoroff89@gmail.com>
2021-09-16 20:22:11 +0300

    update

--- src/StacksAndQueues01/Excercise/BalancedParentheses05.java
+++ src/StacksAndQueues01/Excercise/BalancedParentheses05.java
@@ -7,26 +7,37 @@ public class BalancedParentheses05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<String> parentheses = new ArrayDeque<>();
+        ArrayDeque<Character> opened = new ArrayDeque<>();
         String input = scanner.nextLine();
+        boolean areBalanced = false;
 
         for (int i = 0; i < input.length(); i++) {
-            if (i < input.length() / 2) {
-                parentheses.push(String.valueOf(input.charAt(i)));
-            } else {
-                if (input.charAt(i) == ')' && parentheses.peek().equals("(")) {
-                    parentheses.pop();
-                } else if (input.charAt(i) == '}' && parentheses.peek().equals("{")) {
-                    parentheses.pop();
-                } else if (input.charAt(i) == ']' && parentheses.peek().equals("[")) {
-                    parentheses.pop();
+            char current = input.charAt(i);
+            if (current == '(' || current == '[' || current == '{') {
+                opened.push(current);
+            } else if (current == ')' || current == ']' || current == '}') {
+                if (opened.isEmpty()){
+                    areBalanced = false;
+                    break;
+                }
+                char lastOpenedBracket = opened.pop();
+                if (lastOpenedBracket == '(' && current == ')') {
+                    areBalanced = true;
+                } else if (lastOpenedBracket == '[' && current == ']') {
+                    areBalanced = true;
+                } else if (lastOpenedBracket == '{' && current == '}') {
+                    areBalanced = true;
+                } else {
+                    areBalanced = false;
+                    break;
                 }
             }
 
+
         }
-        if (parentheses.isEmpty()){
+        if (areBalanced) {
             System.out.println("YES");
-        }else {
+        } else {
             System.out.println("NO");
         }
     }
