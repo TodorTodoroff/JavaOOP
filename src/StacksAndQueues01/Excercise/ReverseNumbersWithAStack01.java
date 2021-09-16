8774cabee9d519f12a753ec492a772b7f0c4a6ec
Az <todortodoroff89@gmail.com>
2021-09-16 18:38:06 +0300

    update

---
+++ src/StacksAndQueues01/Excercise/ReverseNumbersWithAStack01.java
@@ -0,0 +1,23 @@
+package StacksAndQueues01.Excercise;
+
+import java.util.ArrayDeque;
+
+import java.util.Scanner;
+
+public class ReverseNumbersWithAStack01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+        String[] input = scanner.nextLine().split("\\s+");
+
+        for (String element : input) {
+            numbers.push(Integer.parseInt(element));
+        }
+
+        for (Integer element : numbers) {
+            System.out.print(element + " ");
+        }
+
+    }
+}
