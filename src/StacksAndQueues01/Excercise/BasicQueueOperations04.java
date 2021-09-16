34ae0a9d82245f3b7370e283b2c4af29a3663cf4
Az <todortodoroff89@gmail.com>
2021-09-16 19:12:34 +0300

    update

---
+++ src/StacksAndQueues01/Excercise/BasicQueueOperations04.java
@@ -0,0 +1,12 @@
+package StacksAndQueues01.Excercise;
+
+import java.util.Scanner;
+
+public class BasicQueueOperations04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
--- src/StacksAndQueues01/Excercise/MaximumElement03.java
+++ src/StacksAndQueues01/Excercise/MaximumElement03.java
@@ -1,12 +1,32 @@
 package StacksAndQueues01.Excercise;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class MaximumElement03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+        int n = Integer.parseInt(scanner.nextLine());
 
+        for (int i = 0; i < n; i++) {
+            int command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    int pushNum = scanner.nextInt();
+                    numbers.push(pushNum);
+                    break;
+                case 2:
+                    numbers.pop();
+                    break;
+                case 3:
+                    System.out.println(Collections.max(numbers));
+                    break;
+            }
+
+        }
 
     }
 }
