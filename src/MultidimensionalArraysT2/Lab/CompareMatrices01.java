00d6b172833852dff19e32887869d640d91c0236
Az <todortodoroff89@gmail.com>
2021-09-17 18:58:51 +0300

    update

---
+++ src/MultidimensionalArraysT2/Lab/CompareMatrices01.java
@@ -0,0 +1,49 @@
+package MultidimensionalArraysT2.Lab;
+
+
+import java.util.Scanner;
+
+public class CompareMatrices01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        int[][] matrixOne = new int[n][x];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                matrixOne[i][j] = scanner.nextInt();
+            }
+        }
+
+        n = scanner.nextInt();
+        x = scanner.nextInt();
+        int[][] matrixTwo = new int[n][x];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                matrixTwo[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(areEqual(matrixOne,matrixTwo));
+
+
+    }
+
+    public static boolean areEqual(int[][] one, int[][] two) {
+        if (one.length != two.length)
+            return false;
+        for (int i = 0; i < one.length; i++) {
+            if (one[i].length != two[i].length)
+                return false;
+            for (int j = 0; j < two.length; j++) {
+                if (one[j].length != two[j].length)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
