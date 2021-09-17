a7bc6d57b4925ec1df9043e54f39af9e62e7bd92
Az <todortodoroff89@gmail.com>
2021-09-17 19:40:54 +0300

    update

---
+++ src/MultidimensionalArraysT2/Lab/IntersectionOfTwoMatrices03.java
@@ -0,0 +1,12 @@
+package MultidimensionalArraysT2.Lab;
+
+import java.util.Scanner;
+
+public class IntersectionOfTwoMatrices03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
