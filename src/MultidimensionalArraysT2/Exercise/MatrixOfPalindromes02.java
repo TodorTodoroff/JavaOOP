9c5160bbafe6b1e8c5360053e8f126befeaeef23
Az <todortodoroff89@gmail.com>
2021-09-20 19:46:28 +0300

    update

--- src/MultidimensionalArraysT2/Exercise/MatrixOfPalindromes02.java
+++ src/MultidimensionalArraysT2/Exercise/MatrixOfPalindromes02.java
@@ -20,7 +20,9 @@ public class MatrixOfPalindromes02 {
 
     private static void printMatrix(String[][] matrix) {
         for (String[] strings : matrix) {
-            System.out.println();
+            System.out.println(Arrays.toString(strings).replace("[","")
+                    .replace("]","")
+                    .replace(","," "));
         }
 
     }
