d0f177cb38cee4ab812511d188a8eef8d9b478a3
Az <todortodoroff89@gmail.com>
2021-09-21 23:23:31 +0300

    update

--- src/SetsAndMapsAdvancedT3/Lab/CitiesByContinentAndCountry07.java
+++ src/SetsAndMapsAdvancedT3/Lab/CitiesByContinentAndCountry07.java
@@ -1,13 +1,35 @@
 package SetsAndMapsAdvancedT3.Lab;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CitiesByContinentAndCountry07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, Map<String, List<String>>> allTogether = new LinkedHashMap<>();
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+            String continent = input.split("\\s+")[0];
+            String country = input.split("\\s+")[1];
+            String city = input.split("\\s+")[2];
+
+            allTogether.putIfAbsent(continent, new LinkedHashMap<>());
+            allTogether.get(continent).putIfAbsent(country, new ArrayList<>());
+            allTogether.get(continent).get(country).add(city);
+
+        }
+allTogether.entrySet().stream()
+        .forEach(entry -> {
+            System.out.println(entry.getKey() + ":");
+            entry.getValue().entrySet().stream()
+                    .forEach(ie -> {
+                        System.out.println("  " + ie.getKey() + " -> "
+                        + String.join(", ",ie.getValue()));
+                    });
+        });
 
     }
 }
