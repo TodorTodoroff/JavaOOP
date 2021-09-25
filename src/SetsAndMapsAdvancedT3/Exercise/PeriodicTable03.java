a7f84aa0dd6382cb4bfb0115af1bb70bbc108d47
Az <todortodoroff89@gmail.com>
2021-09-25 10:17:22 +0300

    update

--- src/SetsAndMapsAdvancedT3/Exercise/PeriodicTable03.java
+++ src/SetsAndMapsAdvancedT3/Exercise/PeriodicTable03.java
@@ -1,12 +1,20 @@
 package SetsAndMapsAdvancedT3.Exercise;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class PeriodicTable03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> elements = new TreeSet<>();
 
+        while (n-- > 0) {
+            String[] input = scanner.nextLine().split("\\s+");
 
+            elements.addAll(Arrays.asList(input));
+
+        }
+       elements.forEach(e -> System.out.printf("%s ",e));
     }
 }
