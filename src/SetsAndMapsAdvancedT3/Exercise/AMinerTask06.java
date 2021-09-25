1d2067a8fb2de5f0591d4ae271fcc567d59157aa
Az <todortodoroff89@gmail.com>
2021-09-25 13:20:52 +0300

    update

--- src/SetsAndMapsAdvancedT3/Exercise/AMinerTask06.java
+++ src/SetsAndMapsAdvancedT3/Exercise/AMinerTask06.java
@@ -1,12 +1,28 @@
 package SetsAndMapsAdvancedT3.Exercise;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AMinerTask06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+        Map<String, Integer> resources = new LinkedHashMap<>();
+        int lineCounter = 1;
+        String element = "";
 
-
+        while (!input.equals("stop")) {
+            if (lineCounter % 2 != 0) {
+                resources.putIfAbsent(input, 0);
+                element = input;
+            } else {
+                resources.put(element, resources.get(element) + Integer.parseInt(input));
+            }
+            lineCounter++;
+            input = scanner.nextLine();
+        }
+        resources.forEach((key, value) -> System.out.println(key + " -> " + value));
     }
 }
