c3b78192fa4013a60bb6d19906b81880ed5d8c0c
Az <todortodoroff89@gmail.com>
2021-09-26 17:44:06 +0300

    update

--- src/MultidimensionalArraysT2/Exercise/ReverseMatrixDiagonals11.java
+++ src/MultidimensionalArraysT2/Exercise/ReverseMatrixDiagonals11.java
@@ -7,24 +7,38 @@ public class ReverseMatrixDiagonals11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
-        int rows = Integer.parseInt(input.split("\\s+")[0]);
-        int cols = Integer.parseInt(input.split("\\s+")[1]);
+        int[] dimensions = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int rows = dimensions[0], cols = dimensions[1];
 
         int[][] matrix = new int[rows][cols];
-        matrix = readMatrix(scanner, matrix);
 
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+        int row = rows - 1;
+        int col = cols - 1;
 
+        while (row != -1){
+            int r = row;
+            int c = col;
+            while (c < cols && r >= 0){
+                System.out.print(matrix[r--][c++] + " ");
+            }
+            System.out.println();
+            col--;
+            if(col == -1){
+                col = 0;
+                row--;
+            }
+        }
     }
 
-    private static int[][] readMatrix(Scanner scanner, int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            int[] arr = Arrays.stream(scanner.nextLine()
-                            .split("\\s+"))
-                    .mapToInt(e -> Integer.parseInt(e))
-                    .toArray();
-            matrix[i] = arr;
-        }
-        return matrix;
+    private static boolean isInbounds(int row, int col, int [][] matrix) {
+        return row < 0 || col < 0 || row > matrix.length - 1 || col > matrix[0].length - 1;
     }
 }
