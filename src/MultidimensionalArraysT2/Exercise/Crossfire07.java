b83ec05df0fe4d0788d9cc229225ef31ba653942
Az <todortodoroff89@gmail.com>
2021-09-26 17:41:06 +0300

    update

--- src/MultidimensionalArraysT2/Exercise/Crossfire07.java
+++ src/MultidimensionalArraysT2/Exercise/Crossfire07.java
@@ -1,79 +1,64 @@
 package MultidimensionalArraysT2.Exercise;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Crossfire07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] dimensions = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
-        String input = scanner.nextLine();
-        int rows = Integer.parseInt(input.split("\\s+")[0]);
-        int cols = Integer.parseInt(input.split("\\s+")[1]);
+        int rows = dimensions[0];
+        int cols = dimensions[1];
 
-        int[][] matrix = new int[rows][cols];
+        List<List<Integer>> matrix = readMatrix(rows, cols);
 
-        matrix = fillMatrix(matrix);
-        input = scanner.nextLine();
-        while (!input.equals("Nuke it from orbit")) {
-            int rowBlast = Integer.parseInt(input.split("\\s+")[0]);
-            int colBlast = Integer.parseInt(input.split("\\s+")[1]);
-            int radius = Integer.parseInt(input.split("\\s+")[2]);
+        String command = scanner.nextLine();
+        while (!command.equals("Nuke it from orbit")) {
+            String[] tokens = command.split("\\s+");
 
-            for (int i = rowBlast - radius; i <= rowBlast + radius; i++) {
-                if (i < matrix.length && i >= 0 && i < matrix[i].length) {
-                    matrix[i][colBlast] = -666;
+            int startRow = Integer.parseInt(tokens[0]);
+            int startCol = Integer.parseInt(tokens[1]);
+            int radius = Integer.parseInt(tokens[2]);
+
+            for (int i = startRow - radius; i <= startRow + radius; i++) {
+                if (isInRange(i, startCol, matrix) && i != startRow) {
+                    matrix.get(i).remove(startCol);
                 }
             }
 
-            for (int i = colBlast - radius; i <= colBlast + radius; i++) {
-                if (i < matrix[rowBlast].length && i >= 0) {
-                    matrix[rowBlast][i] = -666;
+            for (int i = startCol + radius; i >= startCol - radius; i--) {
+                if (isInRange(startRow, i, matrix)) {
+                    matrix.get(startRow).remove(i);
                 }
             }
-            matrix = removeElements(matrix);
 
-            input = scanner.nextLine();
+            matrix.removeIf(List::isEmpty);
+            command = scanner.nextLine();
         }
 
-        printMatrix(matrix);
-
-    }
-
-    private static int[][] removeElements(int[][] matrix) {
-        int[][] newMatrix = new int[matrix.length][];
-        for (int i = 0; i < matrix.length; i++) {
-            List<Integer> deiba = new ArrayList<>();
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] != -666) {
-                    deiba.add(matrix[i][j]);
-                }
-            }
-            newMatrix[i] = deiba.stream().mapToInt(Integer::intValue).toArray();
-        }
-        return newMatrix;
-    }
-
-    private static void printMatrix(int[][] matrix) {
-        for (int[] ints : matrix) {
-            for (int anInt : ints) {
-                if (anInt != -666)
-                    System.out.print(anInt + " ");
+        for (List<Integer> integers : matrix) {
+            for (Integer number : integers) {
+                System.out.print(number + " ");
             }
             System.out.println();
         }
-
     }
 
-    private static int[][] fillMatrix(int[][] matrix) {
-        int counter = 1;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = counter++;
+    public static List<List<Integer>> readMatrix(int rows, int cols) {
+
+        List<List<Integer>> matrix = new ArrayList<>();
+        int startNumber = 1;
+
+        for (int row = 0; row < rows; row++) {
+            matrix.add(new ArrayList<>());
+            for (int col = 0; col < cols; col++) {
+                matrix.get(row).add(startNumber++);
             }
         }
         return matrix;
+    }
+
+    private static boolean isInRange(int startRow, int startCol, List<List<Integer>> matrix) {
+        return startRow >= 0 && startRow < matrix.size() && startCol >= 0 && startCol < matrix.get(startRow).size();
     }
 }
