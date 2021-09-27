3b3fc76428e1d3ed693ccc87e2291b7fdd7537ce
Az <todortodoroff89@gmail.com>
2021-09-27 18:41:07 +0300

    update

---
+++ src/StreamsFilesAndDirectoriesT4/Exercise/CountCharacterTypes03.java
@@ -0,0 +1,11 @@
+package StreamsFilesAndDirectoriesT4.Exercise;
+
+import java.io.IOException;
+
+public class CountCharacterTypes03 {
+    public static void main(String[] args) throws IOException {
+
+
+
+    }
+}
--- src/StreamsFilesAndDirectoriesT4/Exercise/SumBytes02.java
+++ src/StreamsFilesAndDirectoriesT4/Exercise/SumBytes02.java
@@ -1,26 +1,32 @@
 package StreamsFilesAndDirectoriesT4.Exercise;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SumBytes02 {
     public static void main(String[] args) throws IOException {
 
-        Path path = Path.of("C:\\Users\\Az\\Desktop\\JavaProjects\\src\\StreamsFilesAndDirectoriesT4\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt") ;
+        Path path = Path.of("C:\\Users\\Az\\Desktop\\JavaProjects\\src\\StreamsFilesAndDirectoriesT4\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt");
         List<String> allLines = Files.readAllLines(path);
+        List<String> capitals = new ArrayList<>();
 
         int sum = 0;
-        for(String line : allLines){
-
+        for (String line : allLines) {
+            StringBuilder currentString = new StringBuilder();
             for (int i = 0; i < line.length(); i++) {
                 char current = line.charAt(i);
-                sum += current;
+                if (current > 96 && current < 123) {
+                    current -= 32;
+                }
+                currentString.append(current);
             }
+            capitals.add(currentString.toString());
         }
-        System.out.println(sum);
-
+        Files.write(Path.of("output.txt"),capitals);
 
     }
 }
