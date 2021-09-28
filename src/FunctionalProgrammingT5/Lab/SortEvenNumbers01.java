e780762304d19bb316b82c8d246805b82bf8518f
Az <todortodoroff89@gmail.com>
2021-09-28 19:40:25 +0300

    update

---
+++ src/FunctionalProgrammingT5/Lab/SortEvenNumbers01.java
@@ -0,0 +1,27 @@
+package FunctionalProgrammingT5.Lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SortEvenNumbers01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> evenNumbers = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+
+//        System.out.println(String.join(", ", numbers.toString().replace("[","").replace("]","")));
+//        numbers.sort(Integer::compareTo);
+//        System.out.println(String.join(", ", numbers.toString().replace("[","").replace("]","")));
+        String firstOutput = evenNumbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(firstOutput);
+
+        String secondOutput = evenNumbers.stream().sorted(Integer::compareTo).map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(secondOutput);
+    }
+}
---
+++ src/FunctionalProgrammingT5/Lab/SumNumbers02.java
@@ -0,0 +1,11 @@
+package FunctionalProgrammingT5.Lab;
+
+import java.util.Scanner;
+
+public class SumNumbers02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
