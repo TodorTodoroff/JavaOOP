34a4c9657d27c62f51b8d057c2064343a2d72587
Az <todortodoroff89@gmail.com>
2021-10-04 21:01:54 +0300

    update

---
+++ src/FunctionalProgrammingT5/Exercise/ListOfPredicates09.java
@@ -0,0 +1,33 @@
+package FunctionalProgrammingT5.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+public class ListOfPredicates09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Integer> numbersForDivision = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        BiFunction<List<Integer>, Integer, Boolean> isDivisible = ((list, number) -> {
+            for (int numberInList : list) {
+                if (number % numberInList != 0) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        for (int number = 1; number <= n; number++) {
+            if(isDivisible.apply(numbersForDivision, number)) {
+                System.out.print(number + " ");
+            }
+
+        }
+
+    }
+
+}
