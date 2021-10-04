43910e4695ded927e84564d82ce52f925b46009e
Az <todortodoroff89@gmail.com>
2021-10-04 19:50:36 +0300

    update

---
+++ src/FunctionalProgrammingT5/Exercise/PredicateForNames06.java
@@ -0,0 +1,23 @@
+package FunctionalProgrammingT5.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateForNames06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        Predicate<String> isLonger = x -> x.length() <= n;
+
+        names.stream().filter(isLonger).forEach(System.out::println);
+
+    }
+}
