bfd6f03b9a55771fd946171588837e4ef411134b
Az <todortodoroff89@gmail.com>
2021-10-04 18:31:39 +0300

    update

---
+++ src/FunctionalProgrammingT5/Exercise/KnightsOfHonor02.java
@@ -0,0 +1,24 @@
+package FunctionalProgrammingT5.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class KnightsOfHonor02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        Consumer<String> printNames = name -> System.out.println("Sir " + name);
+
+        for (String nam : names) {
+            printNames.accept(nam);
+        }
+
+
+    }
+}
