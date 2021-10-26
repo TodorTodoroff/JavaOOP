5e23d0e616a1b35e22909b0fcc67ad52c5b08549
Az <todortodoroff89@gmail.com>
2021-10-26 19:03:12 +0300

    update

---
+++ JavaOOP/src/WorkingWithAbstractionT1/Lab/PointInRectangle02.java
@@ -0,0 +1,10 @@
+package WorkingWithAbstractionT1.Lab;
+
+public class PointInRectangle02 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ JavaOOP/src/WorkingWithAbstractionT1/Lab/RhombusOfStars01.java
@@ -0,0 +1,42 @@
+package WorkingWithAbstractionT1.Lab;
+
+import java.util.Scanner;
+
+public class RhombusOfStars01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        printTop(n);
+
+        printLineOfSpacesOrStars(n, "* ");
+
+        System.out.println();
+
+        printBottom(n);
+    }
+
+    private static void printBottom(int n) {
+        for (int i = 1; i < n; i++) {
+            printLineOfSpacesOrStars(i, " ");
+            printLineOfSpacesOrStars(n - i, "* ");
+            System.out.println();
+        }
+    }
+
+    private static void printTop(int n) {
+        for (int i = 1; i < n; i++) {
+            printLineOfSpacesOrStars(n - i, " ");
+            printLineOfSpacesOrStars(i, "* ");
+            System.out.println();
+        }
+    }
+
+    private static void printLineOfSpacesOrStars(int i2, String s) {
+        for (int j = 0; j < i2; j++) {
+            System.out.print(s);
+        }
+    }
+}
