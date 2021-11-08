d0e00658e6c3e045d938c9cd676af4bd93ef1459
Az <todortodoroff89@gmail.com>
2021-11-08 12:56:45 +0200

    update

---
+++ JavaOOP/src/InterfacesAndAbstractionT4/Lab/sayHello/Chinese.java
@@ -0,0 +1,13 @@
+package InterfacesAndAbstractionT4.Lab.sayHello;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
---
+++ JavaOOP/src/InterfacesAndAbstractionT4/Lab/sayHello/Person.java
@@ -0,0 +1,7 @@
+package InterfacesAndAbstractionT4.Lab.sayHello;
+
+public interface Person {
+    String getName();
+
+    String sayHello();
+}
